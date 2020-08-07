96599443bd4b56960970126107dfeae9fa563c49
Justin Lowen <Justin.G.Lowen@Gmail.com>
2020-08-07 10:33:58 -0500

    Refactor beer API call for readablility

--- index.js
+++ index.js
@@ -3,17 +3,30 @@ const urlBase = "https://api.punkapi.com/v2/beers";
 
 async function getBeers() {
     // fetch & process
-    const beerPromise = await fetch("https://api.punkapi.com/v2/beers");
+    const beerPromise = await fetch(urlBase);
     const beers = await beerPromise.json();
 
     // render data
-    const beerFragment = document.createDocumentFragment();
+    const beersDiv = document.querySelector('.beers');
+
+    let beerHtml = "";
+
     beers.forEach(beer => {
-        const beerHeading = document.createElement('h3');
-        beerHeading.textContent = beer.name;
-        beerFragment.appendChild(beerHeading);
-    })
-    document.getElementsByClassName('beers')[0].appendChild(beerFragment);
+        beerHtml += `
+        <div class='beer-wrapper card'>
+            <div class='beer'>
+                <img class='beer__img' src="${beer.image_url}">
+                <h3>${beer.name}</h3>
+                <span class='beer__info'>
+                    <span>ABV: ${beer.abv}%</span>
+                    <span>IBU: ${beer.ibu}</span>
+                </span>
+            </div>
+        </div>
+       `;
+    });
+
+    beersDiv.innerHTML = beerHtml;
 }
 
 // initial get
